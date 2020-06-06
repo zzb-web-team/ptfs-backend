2cd9aa3fe45a535f927f2652c2ec8a88772ab67c
wuyazi <wuyazi@wuyaziwuyaziwuyaziwuyaziwuyazi.com>
2020-06-06 11:35:30 +0800

    上报

--- application/api/controller/Minerearn.php
+++ application/api/controller/Minerearn.php
@@ -312,33 +312,34 @@ class Minerearn extends Common
             "userip" => request()->ip(), 
             "appversion" => $data['version']
         );
-        $curl = curl_init();
-        //设置抓取的url
-        curl_setopt($curl, CURLOPT_URL, "http://39.100.131.247:3469/api/ptfs");
-        //设置头文件的信息作为数据流输出
-        curl_setopt($curl, CURLOPT_HEADER, 1);
-        //设置获取的信息以文件流的形式返回，而不是直接输出。
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        //设置post方式提交
-        curl_setopt($curl, CURLOPT_POST, 1);
-        //设置post数据
-        $post_data['method'] = 'ipfs_data_center/app_turnon_log';
-        $post_data['data'] = $param;
+        return self::loadApiData("ipfs_data_center/app_turnon_log", $param);
+        // $curl = curl_init();
+        // //设置抓取的url
+        // curl_setopt($curl, CURLOPT_URL, "http://39.100.131.247:3469/api/ptfs");
+        // //设置头文件的信息作为数据流输出
+        // curl_setopt($curl, CURLOPT_HEADER, 1);
+        // //设置获取的信息以文件流的形式返回，而不是直接输出。
+        // curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        // //设置post方式提交
+        // curl_setopt($curl, CURLOPT_POST, 1);
+        // //设置post数据
+        // $post_data['method'] = 'ipfs_data_center/app_turnon_log';
+        // $post_data['data'] = $param;
         
-        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post_data));
-        //执行命令
-        $response = curl_exec($curl);
-        //显示获得的数据
-        if (curl_getinfo($curl, CURLINFO_HTTP_CODE) == '200') {
-            $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
-            $header = substr($response, 0, $headerSize);
-            $body = substr($response, $headerSize);
-        } else {
-            $body = $response;
-        }
-        //关闭URL请求
-        curl_close($curl);
-        return $body;
+        // curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($post_data));
+        // //执行命令
+        // $response = curl_exec($curl);
+        // //显示获得的数据
+        // if (curl_getinfo($curl, CURLINFO_HTTP_CODE) == '200') {
+        //     $headerSize = curl_getinfo($curl, CURLINFO_HEADER_SIZE);
+        //     $header = substr($response, 0, $headerSize);
+        //     $body = substr($response, $headerSize);
+        // } else {
+        //     $body = $response;
+        // }
+        // //关闭URL请求
+        // curl_close($curl);
+        // return $body;
     }
 
     public function ptfs_query_node_grade()
