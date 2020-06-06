57a9955a6b8a19d63b79dd3c6519e889b4c154eb
mYangfan <1419574905@qq.com>
2020-06-06 17:11:06 +0800

    6-6commit so boring,commit some changes

--- application/cloud/controller/Common.php
+++ application/cloud/controller/Common.php
@@ -145,4 +145,79 @@ class Common extends Controller
         
     }
 
+    public static function getIp()
+    {
+        if (isset($_SERVER)) {
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                $realip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
+                $realip = $_SERVER['HTTP_CLIENT_IP'];
+            } else {
+                $realip = $_SERVER['REMOTE_ADDR'];
+            }
+        } else {
+            if (getenv("HTTP_X_FORWARDED_FOR")) {
+                $realip = getenv("HTTP_X_FORWARDED_FOR");
+            } elseif (getenv("HTTP_CLIENT_IP")) {
+                $realip = getenv("HTTP_CLIENT_IP");
+            } else {
+                $realip = getenv("REMOTE_ADDR");
+            }
+        }
+        return $realip;
+    }
+
+    public static function judge_password($password){
+        $score = 0;
+        if(!empty($password)){ //接收的值
+            $str = $password;
+        } else{
+            $str = '';
+        }
+        if(preg_match("/[0-9]+/",$str))
+        {
+            $score ++;
+        }
+        if(preg_match("/[0-9]{3,}/",$str))
+        {
+            $score ++;
+        }
+        if(preg_match("/[a-z]+/",$str))
+        {
+            $score ++;
+        }
+        if(preg_match("/[a-z]{3,}/",$str))
+        {
+            $score ++;
+        }
+        if(preg_match("/[A-Z]+/",$str))
+        {
+            $score ++;
+        }
+        if(preg_match("/[A-Z]{3,}/",$str))
+        {
+            $score ++;
+        }
+        if(preg_match("/[_|\-|+|=|*|!|@|#|$|%|^|&|(|)]+/",$str))
+        {
+            $score += 2;
+        }
+        if(preg_match("/[_|\-|+|=|*|!|@|#|$|%|^|&|(|)]{3,}/",$str))
+        {
+            $score ++ ;
+        }
+        if(strlen($str) >= 10)
+        {
+            $score ++;
+        }
+        if($score>=1 && $score<=3){
+            return ['lv' => 1,'lvmsg' => '弱'];
+        }else if($score>=4 && $score<=6){
+            return ['lv' => 2,'lvmsg' => '中'];
+        }else if($score>=7){
+            return ['lv' => 3,'lvmsg' => '强'];
+        }
+
+    }
+
 }
--- application/cloud/controller/System.php
+++ application/cloud/controller/System.php
@@ -1,6 +1,10 @@
 <?php
 namespace app\cloud\controller;
+
+use think\facade\Cache;
 use think\Validate;
+
+require '../extend/PHPGangsta/GoogleAuthenticator.php';
 
 class System extends Common
 { 
@@ -37,12 +41,125 @@ class System extends Common
                     return json(['status' => -900, 'msg' => '用户已被禁用']);
                 }
                 unset($user['password']);
-                return json(['status' => 0, 'msg' => $user]);
+                $list = Cache::store('redis') -> get('ipfs:ip:list', json_encode([]));
+                $list = json_decode($list, true);
+                $result = self::judge_password($data['password']);
+                if (!empty($list)) {
+                    $column = array_column($list, 'ip');
+                    $ip = self::getIp();
+                    $res = array_search($ip, $column);
+                    if (!$res) {
+                        return json(['status' => -900, 'msg' => '非法ip']);
+                    } else if (Cache::store('redis')->get('cloud:admin_status:' . $user['id'])) {
+                        $token = md5(time() . '_jkhgasdgjkdsa');
+                        Cache::store('redis')->set('cloud:admin_token:' . $token, $user['id'], 60);
+                        return json(['status' => 1, 'token' => $token, 'msg' => '请输入code', 'passlv' => $result['lv'], 'lvmsg' => $result['lvmsg']]);
+                    } else {
+                        return json(['status' => 0, 'msg' => $user, 'passlv' => $result['lv'], 'lvmsg' => $result['lvmsg'], 'google' => 0]);
+                    }
+                } else if (Cache::store('redis')->get('cloud:admin_status:' . $user['id'])) {
+                    $token = md5(time() . '_jkhgasdgjkdsa');
+                    Cache::store('redis')->set('cloud:admin_token:' . $token, $user['id'], 60);
+                    return json(['status' => 1, 'token' => $token, 'msg' => '请输入code', 'passlv' => $result['lv'], 'lvmsg' => $result['lvmsg']]);
+                } else {
+                    return json(['status' => 0, 'msg' => $user, 'passlv' => $result['lv'], 'lvmsg' => $result['lvmsg'], 'google' => 0]);
+                }
             }
             return json(['status' => -900, 'msg' => '找不到该用户']);
         }
         return json($return_data);
-        
+    }
+
+    //用户输入账号密码且校验通过后验证code
+    public function check_login()
+    {
+        $data = input('post.');
+        $validation = new Validate([
+            'token' => 'require',
+            'code' => 'require',
+        ]);
+        //验证表单
+        if (!$validation->check($data)) {
+            return json(['status' => -900, 'err_code' => -900, 'msg' => $validation->getError()]);
+        }
+        $id = Cache::store('redis')->get('cloud:admin_token:' . $data['token']);
+        if (empty($id)) {
+            return json(['status' => -900, 'msg' => 'token无效']);
+        }
+        $param = array(
+            "page" => 0,
+            "page_size" => 10,
+            "tb_name" => 'cloud_system_user',
+            "col_name" => "*",
+            "where" => "id=" . $id,
+            "order" => 'id desc',
+        );
+        $return_data = self::loadApiData("store/find_table", $param);
+        if (!$return_data) {
+            return json(['status' => -900, 'msg' => '服务器可能开小差去了']);
+        }
+        $return_data = json_decode($return_data, true);
+        $user = $return_data['result']['cols'][0];
+
+        $secret = Cache::store('redis')->get('cloud:admin_login:' . $id);
+        $google_auth = new \PHPGangsta_GoogleAuthenticator();
+        $code = $google_auth->getCode($secret);
+        $check_result = $google_auth->verifyCode($secret, $data['code'], 2);
+        if ($check_result) {
+            return json(['status' => 0, 'usermsg' => $user, 'msg' => '登陆成功', 'google' => 1]);
+        } else {
+            return json(['status' => -900, 'msg' => 'code错误']);
+        }
+    }
+
+     //用户第一次开通二次验证流程
+    //status 0未开通 1已开通
+    public function bind_login()
+    {
+        $data = input('post.');
+        $param = array(
+            "page" => 0,
+            "page_size" => 10,
+            "tb_name" => 'cloud_system_user',
+            "col_name" => "*",
+            "where" => "id=" . $data['id'],
+            "order" => 'id desc',
+        );
+        $return_data = self::loadApiData("store/find_table", $param);
+        $return_data = json_decode($return_data, true);
+        if(!isset($return_data['result']['cols'][0])){
+            return json(['status' => -900, 'msg' => '没有该用户']);
+        }
+        $user = $return_data['result']['cols'][0];
+        $google_auth = new \PHPGangsta_GoogleAuthenticator();
+        if (!isset($data['code'])) {
+            //获取二维码
+            if (Cache::store('redis')->has('cloud:admin_login:' . $data['id'])) {
+                //已开通
+                $secret = Cache::store('redis')->get('cloud:admin_login:' . $data['id']);
+            } else {
+                //未开通二次验证
+                $secret = $google_auth->createSecret();
+                Cache::store('redis')->set('cloud:admin_login:' . $data['id'], $secret);
+            }
+            writelog("id:" . $user['id'] . "username:" . $user['username'], 'admin/system/bind_login', $secret, $type = "POST"); //详细信息日志
+            $qrcode_url = $google_auth->getQRCodeGoogleUrl($data['id'], $secret);
+            return json([
+                'status' => 0,
+                'msg' => [
+                    'url' => $qrcode_url,
+                    'secret' => $secret,
+                ]]);
+        } else {
+            $secret = Cache::store('redis')->get('cloud:admin_login:' . $data['id']);
+            $check_result = $google_auth->verifyCode($secret, $data['code'], 2);
+            if ($check_result) {
+                Cache::store('redis')->set('cloud:admin_status:' . $data['id'], 1);
+                return json(['status' => 0, 'msg' => '绑定成功']);
+            } else {
+                return json(['status' => -900, 'msg' => '绑定失败']);
+            }
+        }
     }
 
     public function userlist()
